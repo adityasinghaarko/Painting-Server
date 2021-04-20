3698d8c992791f60b97fd4cd34cce5438b68d59e
adityasingha <adityasingha659@gmail.com>
2021-04-20 19:47:28 +0600

    review done

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const fs = require('fs-extra')
 const fileUpload = require('express-fileupload')
 const ObjectId = require('mongodb').ObjectId;
 
-// const admin = require('firebase-admin');
-// var serviceAccount = require("./book-passage-firebase-adminsdk-g8fdb-d75dcf898e.json");
+const admin = require('firebase-admin');
+var serviceAccount = require("./painting-8a9fd-firebase-adminsdk-nmmmo-ba08073fec.json");
 
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount)
-// });
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
 const MongoClient = require('mongodb').MongoClient;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qk4l2.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
@@ -21,6 +21,7 @@ const app = express()
 const port = 5000;
 
 app.use(bodyParser.json())
+app.use(bodyParser.json())
 app.use(cors())
 app.use(express.static('serviceImages'));
 app.use(fileUpload())
@@ -29,21 +30,67 @@ app.use(fileUpload())
 
 
 client.connect(err => {
-  const reviewssCollection = client.db("painting").collection("reviews");
+  const reviewsCollection = client.db("painting").collection("reviews");
   const admins = client.db("painting").collection("admins");
   const servicesCollection = client.db("painting").collection("services");
+  const bookingsCollection = client.db("painting").collection("bookings");
 
+
+
+  //method: GET
   app.get('/reviews', (req, res) => {
-    reviewssCollection.find()
+    reviewsCollection.find()
       .toArray((err, documents) => {
         res.send(documents);
       })
   })
 
+
+  app.get('/bookings', (req, res) => {
+    const queryEmail = req.query.email;
+    const idToken = req.headers.authorization;
+
+    admin
+        .auth()
+        .verifyIdToken(idToken)
+        .then((decodedToken) => {
+          const tokenEmail = decodedToken.email;
+          if (tokenEmail === queryEmail) {
+            bookingsCollection.find({ email: queryEmail })
+              .toArray((errors, documents) => {
+                res.send(documents)
+              })
+          }
+          // ...
+        })
+        .catch((error) => {
+          // Handle error
+        });
+  })
+
+
+
   app.get('/services', (req, res) => {
     servicesCollection.find()
       .toArray((err, documents) => {
         res.send(documents);
+      })
+  })
+
+  app.get('/service/:serviceId', (req, res) => {
+    const serviceId = req.params.serviceId;
+    servicesCollection.find({ _id: ObjectId(`${serviceId}`)})
+    .toArray((error, documents) => {
+      res.send(documents);
+    })
+  })
+
+  app.get('/addAdmin', (req, res) => {
+    const email = req.headers.email;
+    console.log(email);
+    admins.insertOne({ email: email })
+      .then(result => {
+        res.send(result.insertedCount > 0)
       })
   })
 
@@ -54,7 +101,10 @@ client.connect(err => {
         res.send(docs.length > 0);
       })
   })
+  //method: GET
 
+
+  // method:POST
   app.post('/addService', (req, res) => {
     const file = req.files.file;
     const title = req.body.title;
@@ -87,16 +137,39 @@ client.connect(err => {
     })
   })
 
-  app.get('/addAdmin', (req, res) => {
-    const email = req.headers.email;
-    console.log(email);
-    admins.insertOne({email: email})
-    .then(result => {
-      res.send(result.insertedCount > 0)
-    })
+  app.post("/addBooking", (req, res) => {
+    const newBooking = req.body;
+    console.log(req.body)
+    bookingsCollection.insertOne(newBooking)
+      .then(result => {
+        res.send(result.insertedCount > 0)
+      })
+
   })
 
+  app.post("/addReview", (req, res) => {
+    const newReview = req.body;
+    reviewsCollection.insertOne(newReview)
+      .then(result => {
+        res.send(result.insertedCount > 0)
+      })
 
+  })
+  // method:POST
+
+  
+  // method:DELETE
+  app.delete('/deleteService/:serviceId', (req, res) => {
+    const serviceId = req.params.serviceId
+    console.log(serviceId)
+    servicesCollection.findOneAndDelete({ _id: ObjectId(`${serviceId}`) })
+      .then(result => {
+        if (result.value) {
+          res.send(true)
+        }
+      })
+  })
+  //  method:DELETE
 });
 
 
